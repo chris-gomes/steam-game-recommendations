19c84c110f33f81f28b6be1c98ea3ff1926ad50c
chris-gomes <19chrisg97@gmail.com>
2020-03-26 11:38:47 -0400

    fixed preprocessing not removing popular games and now norms by game

--- preprocessing.py
+++ preprocessing.py
@@ -25,33 +25,32 @@ def get_play_data():
 def play_minimum(plays, threshold):
     games_played = plays.groupby("user_id").count().reset_index()
     games_played = games_played[(games_played['amount'] >= threshold)]
-    new_plays = plays.merge(games_played["user_id"])
+    new_plays = plays.merge(games_played['user_id'], on='user_id')
     return new_plays
 
 
 def drop_too_popular(plays, threshold):
-    users_played = plays.groupby("user_id").count().reset_index()
+    users_played = plays.groupby("game_name").count().reset_index()
     users_played = users_played[(users_played['amount'] <= threshold)]
-    new_plays = plays.merge(users_played['user_id'])
+    new_plays = plays.merge(users_played['game_name'], on='game_name')
     return new_plays
 
 
 def min_max_norm(plays):
-    new_plays = plays.merge(plays.groupby('user_id').agg(
-        {'amount': 'min'}).reset_index(), on='user_id')
-    new_plays = new_plays.merge(new_plays.groupby('user_id').agg(
-        {'amount_x': 'max'}).reset_index(), on='user_id')
+    new_plays = plays.merge(plays.groupby('game_name').agg(
+        {'amount': 'min'}).reset_index(), on='game_name')
+    new_plays = new_plays.merge(new_plays.groupby('game_name').agg(
+        {'amount_x': 'max'}).reset_index(), on='game_name')
     new_plays.columns = ['user_id', 'game_name', 'amount', 'min', 'max']
     new_plays['norm_amount'] = (new_plays['amount'] - new_plays['min'].apply(
         np.floor)) / (new_plays['max'] - new_plays['min'].apply(np.floor))
     return new_plays.drop(columns=['min', 'max'])
 
-# TODO check for bugs
 def main():
     plays = get_play_data()
 
     # only keep play times of at least 1 hour
-    plays = plays[plays['amount'] >= 1]
+    # plays = plays[plays['amount'] >= 1]
 
     # only keep users that played at least 5 games
     plays = play_minimum(plays, 5)
