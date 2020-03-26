3d5a86041820867785db5b1d0dca3e26ad7643a3
chris-gomes <19chrisg97@gmail.com>
2020-03-26 13:31:27 -0400

    preprossing randomly picks per user and games most be played by at least 5 users

--- preprocessing.py
+++ preprocessing.py
@@ -29,9 +29,10 @@ def play_minimum(plays, threshold):
     return new_plays
 
 
-def drop_too_popular(plays, threshold):
+def drop_unplayed_or_too_popular(plays, lower_thresh, upper_thresh):
     users_played = plays.groupby("game_name").count().reset_index()
-    users_played = users_played[(users_played['amount'] <= threshold)]
+    users_played = users_played[(users_played['amount'] >= lower_thresh) &
+                                (users_played['amount'] <= upper_thresh)]
     new_plays = plays.merge(users_played['game_name'], on='game_name')
     return new_plays
 
@@ -46,27 +47,39 @@ def min_max_norm(plays):
         np.floor)) / (new_plays['max'] - new_plays['min'].apply(np.floor))
     return new_plays.drop(columns=['min', 'max'])
 
+
 def main():
     plays = get_play_data()
+    print("Read in data: {}".format(plays.shape[0]))
 
-    # only keep play times of at least 1 hour
-    # plays = plays[plays['amount'] >= 1]
+    # remove games that more than 1000 people have played or less than 5
+    plays = drop_unplayed_or_too_popular(plays, 5, 1000)
+    print("Removed too popular games: {}".format(plays.shape[0]))
 
     # only keep users that played at least 5 games
     plays = play_minimum(plays, 5)
-
-    # remove games that more than 1000 people have played
-    plays = drop_too_popular(plays, 1000)
+    print("Removed users with few played games: {}".format(plays.shape[0]))
 
     # normalize the play amounts by the users min and max play times per game
     plays = min_max_norm(plays)
+    print("Added normalized amount by game")
 
     # split into train and test
-    train, test = train_test_split(plays, test_size=0.25)
+    train = pd.DataFrame()
+    test = pd.DataFrame()
+    for name, group in plays.groupby('user_id'):
+        curr_train, curr_test = train_test_split(group, test_size=0.25)
+        train = train.append(curr_train)
+        test = test.append(curr_test)
+
+    if np.array_equal(train['user_id'].unique(), test['user_id'].unique()):
+        print("Train and test properly constructed")
+    print("Created train and test")
 
     # save to csv
     train.to_csv("data\\train-plays.csv", index=False)
     test.to_csv("data\\test-plays.csv", index=False)
+    print("Saved train and test to csvs")
 
 
 main()
